7bda0c021e8c7dec9c1c4172c11b03d888765f4a
Charles FENDT <charles@fendt.nom.fr>
2016-01-21 21:51:49 +0100

    Local repository loader

--- repo-common/src/main/java/org/dederem/common/service/RepositoryPoolService.java
+++ repo-common/src/main/java/org/dederem/common/service/RepositoryPoolService.java
@@ -3,10 +3,16 @@
  */
 package org.dederem.common.service;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.security.GeneralSecurityException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -15,6 +21,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 import javax.ejb.Singleton;
@@ -35,18 +42,21 @@ import lombok.Getter;
  */
 @Singleton
 public final class RepositoryPoolService {
-
+    
     /** Configuration service. */
     @Inject
     private ConfigService configService;
-
+    
     /** List of suites to manage. */
     @Getter
     private final Set<String> suites;
-    
+
     /** List of suites to manage. */
     private final Map<String, SuiteDesc> suitesDesc = new HashMap<>();
-
+    
+    /** TRUE to use /usr/bin/dpkg. */
+    private final boolean useUsrBinDpkg;
+    
     /**
      * Default constructor.
      */
@@ -55,75 +65,161 @@ public final class RepositoryPoolService {
         // use the configuration parameter to manage suites.
         final String[] suites = StringUtils.split(this.configService.getSuites(), " \t,;");
         this.suites = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(suites)));
-
+        
         for (final String suite : this.suites) {
             this.suitesDesc.put(suite, new SuiteDesc());
         }
+        
+        // check if /usr/bin/dpkg exists
+        final File dpkg = new File("/usr/bin/dpkg");
+        this.useUsrBinDpkg = dpkg.exists();
     }
-
+    
     /**
      * Method to reload the pool directory.
      */
     public void reloadCache() {
         final File pool = new File(this.configService.getRepoDir(), "pool");
-
+        
         for (final String suite : this.suites) {
             final File dir = new File(pool, suite);
             final SuiteDesc desc = this.suitesDesc.get(suite);
             desc.setName(suite);
             desc.setDirectory(dir);
-            
+
             // load the suite !
             final Iterator<File> iterateFiles = FileUtils.iterateFiles(dir, new String[] { ".deb" }, true);
             while (iterateFiles.hasNext()) {
                 final File nextDeb = iterateFiles.next();
                 // load the deb
-                final DebPackageDesc elt = this.loadPackageDesc(nextDeb);
-                desc.getPackages().put(elt.getDebPackage(), elt);
+                try {
+                    final DebPackageDesc elt = this.loadPackageDesc(nextDeb);
+                    desc.getPackages().put(elt.getDebPackage(), elt);
+                } catch (final IOException ex) {
+                    ex.printStackTrace(); // FIXME
+                }
             }
         }
     }
-    
-    private DebPackageDesc loadPackageDesc(final File deb) {
+
+    /**
+     * Method to load a file in the pool.
+     * 
+     * @param deb
+     *            Debian package.
+     * @return A The package description object
+     * @throws IOException
+     *             I/O or parsing error.
+     */
+    private DebPackageDesc loadPackageDesc(final File deb) throws IOException {
         final String path = deb.getAbsolutePath();
-        
+
         final DebPackageDesc result = new DebPackageDesc();
         result.setFileName(path);
         result.setFileSize(deb.length());
         result.setPresent(true);
-
+        
+        final Properties props = new Properties();
         final File desc = new File(path + ".desc");
         if (desc.exists()) {
             // load description
+            try (InputStream input = new FileInputStream(desc)) {
+                props.load(input);
+            }
+            result.setPackageArch(props.getProperty("arch"));
+            result.setPackageMd5(props.getProperty("md5sum"));
+            result.setPackageName(props.getProperty("name"));
+            result.setPackageSha1(props.getProperty("sha1sum"));
+            result.setPackageSha256(props.getProperty("sha256sum"));
+            result.setPackageVersion(props.getProperty("version"));
         } else {
             try {
                 // generate the description file.
+                if (this.useUsrBinDpkg) {
+                    // use /usr/bin/dpkg
+                    final Process process = Runtime.getRuntime().exec("/usr/bin/dpkg --info " + deb.getAbsolutePath()
+                            + " control | grep -E '^(Package|Version|Architecture)' | sed 's/\\(^[^:]*\\):\\W*/\\1=/g'");
+                    process.getOutputStream().close();
 
-                // dpkg --info $deb_path control | grep -E "^(Version|Package|Architecture)"
+                    final Map<String, String> map = new HashMap<>();
+                    try (final BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                        final String[] elt1 = StringUtils.split(stdout.readLine(), "=", 2);
+                        map.put(elt1[0], elt1[1]);
+                        final String[] elt2 = StringUtils.split(stdout.readLine(), "=", 2);
+                        map.put(elt2[0], elt2[1]);
+                        final String[] elt3 = StringUtils.split(stdout.readLine(), "=", 2);
+                        map.put(elt3[0], elt3[1]);
+                    }
+
+                    try {
+                        process.waitFor();
+                    } catch (final InterruptedException exc) {
+                        throw new IOException(exc);
+                    }
+                    
+                    result.setPackageName(map.get("Package"));
+                    result.setPackageArch(map.get("Architecture"));
+                    result.setPackageVersion(map.get("Version"));
+                } else {
+                    // parse the filename "by hand"
+                    final String[] elts = StringUtils.split(StringUtils.substringBeforeLast(deb.getName(), "."), '_');
+                    if (elts.length == 3) {
+                        result.setPackageName(elts[0]);
+                        result.setPackageVersion(elts[1]);
+                        result.setPackageArch(elts[2]);
+                    } else {
+                        throw new IOException(new MalformedURLException());
+                    }
+                }
+                
                 this.hashFile(desc, result);
-            } catch (final Exception ex) {
-                ex.printStackTrace();// FIXME logger
+                
+                // Write the description file
+                props.setProperty("name", result.getPackageName());
+                props.setProperty("arch", result.getPackageArch());
+                props.setProperty("version", result.getPackageVersion());
+                props.setProperty("md5sum", result.getPackageMd5());
+                props.setProperty("sha1sum", result.getPackageSha1());
+                props.setProperty("sha256sum", result.getPackageSha256());
+                desc.createNewFile();
+                try (OutputStream out = new FileOutputStream(desc)) {
+                    props.store(out, StringUtils.EMPTY);
+                }
+                
+            } catch (final GeneralSecurityException ex) {
+                throw new IOException(ex);
             }
         }
-        // FIXME
         return result;
     }
-    
+
+    /**
+     * Method to populate the DebPackageDesc object with all the hash of the given file.
+     *
+     * @param file
+     *            File to hash.
+     * @param desc
+     *            The description to populate with MD5, SHA1 and SHA256.
+     * @throws NoSuchAlgorithmException
+     *             This exception is thrown when one of the hash method if not supported.
+     * @throws IOException
+     *             I/O error on the File object.
+     */
     private void hashFile(final File file, final DebPackageDesc desc) throws NoSuchAlgorithmException, IOException {
         try (InputStream inputStream = new FileInputStream(file)) {
             final MessageDigest digestMD5 = MessageDigest.getInstance("MD5");
             final MessageDigest digestSHA1 = MessageDigest.getInstance("SHA1");
             final MessageDigest digestSHA256 = MessageDigest.getInstance("SHA256");
-            
+
             final byte[] bytesBuffer = new byte[4096];
             int bytesRead = -1;
-            
+
             while ((bytesRead = inputStream.read(bytesBuffer)) != -1) {
                 digestMD5.update(bytesBuffer, 0, bytesRead);
                 digestSHA1.update(bytesBuffer, 0, bytesRead);
                 digestSHA256.update(bytesBuffer, 0, bytesRead);
             }
-            
+
             desc.setPackageMd5(DatatypeConverter.printHexBinary(digestMD5.digest()));
             desc.setPackageSha1(DatatypeConverter.printHexBinary(digestSHA1.digest()));
             desc.setPackageSha256(DatatypeConverter.printHexBinary(digestSHA256.digest()));
